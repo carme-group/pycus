47cafedc09e5757b3a75cf0099c2a32b9c4881e9
Moshe Zadka <zadka.moshe@gmail.com>
2020-08-01 10:07:42 -0700

    wrong name

--- src/pycus/_version.py
+++ src/pycus/_version.py
@@ -1,11 +1,11 @@
 """
-Provides picus version information.
+Provides pycus version information.
 """
 
 # This file is auto-generated! Do not edit!
-# Use `python -m incremental.update picus` to change this file.
+# Use `python -m incremental.update pycus` to change this file.
 
 from incremental import Version
 
-__version__ = Version("picus", 20, 7, 0)
+__version__ = Version("pycus", 20, 7, 0)
 __all__ = ["__version__"]
